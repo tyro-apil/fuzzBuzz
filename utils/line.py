7c2f05ddd594885790c1367d7abf2b64ea70711c
tyro-apil <078bct017.apil@pcampus.edu.np>
2025-03-09 00:03:54 +0545

    feat: add Line class and intersection function

---
+++ utils/line.py
@@ -0,0 +1,75 @@
+"""
+utils/line.py
+
+Line class
+"""
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Point:
+    x: float
+    y: float 
+
+class Line:
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+
+        self.isHorizontal: bool = False
+        if start.y == end.y:
+            self.isHorizontal = True
+        
+        self.isVertical: bool = False
+        if start.x == end.x:
+            self.isVertical = True
+        
+        self._slope = self._calculateSlope()
+        self._yIntercept = self._calculateYIntercept()
+    
+    def _calculateSlope(self) -> Optional[float]:
+        if self.isVertical:
+            return None
+        return (self.end.y - self.start.y)/(self.end.x - self.start.x)
+    
+    def _calculateYIntercept(self) -> Optional[float]:
+        if self.isVertical:
+            return None
+        return self.start.y - self._slope*self.start.x
+
+    def calculateX(self, y: float) -> Optional[float]:
+        if self.isHorizontal:
+            return None
+        if self.isVertical:
+            return self.start.x
+        
+        return ((y-self.start.y)*(self.end.x-self.start.x)/(self.end.y-self.start.y)) + self.start.x
+    
+    def calculateY(self, x: float) -> Optional[float]:
+        if self.isVertical:
+            return None
+        if self.isHorizontal:
+            return self.start.y
+        
+        return ((x-self.start.x)*(self.end.y-self.start.y)/(self.end.x-self.start.x)) + self.start.y
+    
+
+def calculateIntersection(line1: Line, line2: Line) -> Optional[Point]:
+    if (line1._slope == line2._slope) or (line1.isVertical and line2.isVertical):
+        return None
+    
+    if line1.isVertical:
+        x = line1.start.x
+        y = line2.calculateY(x)
+        assert y is not None
+        return Point(x=x, y=y)
+    
+    if line2.isVertical:
+        x = line2.start.x
+        y = line1.calculateY(x=x)
+        assert y is not None
+        return Point(x=x, y=y)
+    
+    x = (line2._yIntercept - line1._yIntercept) / (line1._slope - line2._slope)
+    y = line1._slope * x + line1._yIntercept
+    return Point(x=x, y=y)
