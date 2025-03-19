64d251627799fc94bd92fdb0110e97467b84815d
tyro-apil <078bct017.apil@pcampus.edu.np>
2025-03-19 18:08:40 +0545

    fix: provide range in constructor

--- src/linguisticVariable.py
+++ src/linguisticVariable.py
@@ -3,19 +3,19 @@ src/linguisticVariable.py
 
 Represents a linguistic variable with multiple membership functions
 """
-from typing import Dict
+from typing import Dict, List
 
 class LinguisticVariable:
-    def __init__(self, name: str):
+    def __init__(self, name: str, range: List[int]):
         self.name = name
         self.membership_functions: Dict[str, object] = {}
-        self.range = [0, 0]  # [min, max]
+        self.range = range  # [min, max]
     
     def add_membership_function(self, mem_fn):
         self.membership_functions[mem_fn.label] = mem_fn
         # Update range
-        self.range[0] = min(self.range[0], mem_fn.x_coordinates[0])
-        self.range[1] = max(self.range[1], mem_fn.x_coordinates[-1])
+        # self.range[0] = min(self.range[0], mem_fn.x_coordinates[0])
+        # self.range[1] = max(self.range[1], mem_fn.x_coordinates[-1])
     
     def get_membership_degree(self, value: float, label: str) -> float:
         """Get membership degree for a specific label at given value"""
